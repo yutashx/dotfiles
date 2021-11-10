6e82c9e1f0e69fe1ea4991e899eb4aacd6a5773f
yutashx <yutashx@gmail.com>
2021-11-10 21:28:09 +0900

    fix docker installer

--- etc/init/ubuntu/docker.sh
+++ etc/init/ubuntu/docker.sh
@@ -15,4 +15,9 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+groupadd docker
+gpasswd -a $USER docker
+systemctl restart docker
+
+echo "need restart"
